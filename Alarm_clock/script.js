5f24a77a25007c6a242c839272eddf40a765b1ec
Shivang Yadav <shivangyadav121@gmail.com>
2023-06-20 00:44:46 +0530

    updated

--- Alarm_clock/script.js
+++ Alarm_clock/script.js
@@ -1,32 +1,42 @@
+let audio = new Audio(
+  "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"
+);
+let intervalId;
 setInterval(() => {
-  let d = new Date().toTimeString();
-  currTime.innerHTML = d;
+let d = new Date().toTimeString();
+currTime.innerHTML = d;
 });
 time.innerHTML = `
 <div class="set-time">
-  <input type="time" id="timeInput" required>
-  <input type="submit" value="Set Alarm" id="btn">
+<input type="time" id="timeInput" required>
+<input type="submit" value="Set Alarm" id="btn" class="button">
+<input type="submit" value="Stop" id="stp" class="button">
 </div>
 `;
 
 let button = document.getElementById("btn");
 let timeInput = document.getElementById("timeInput");
+
 button.addEventListener("click", function () {
-  let timeValue = timeInput.value;
-  let inputHours = timeValue.split(":")[0];
-  let inputMin = timeValue.split(":")[1];
-  alert(`Your Alarm is set for ${inputHours}:${inputMin}`);
-  let audio = new Audio(
-    "http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"
-  );
+let timeValue = timeInput.value;
+let inputHours = timeValue.split(":")[0];
+let inputMin = timeValue.split(":")[1];
+alert(`Your Alarm is set for ${inputHours}:${inputMin}`);
 
-  setInterval(() => {
-    let d = new Date();
-    let currHours = d.getHours();
-    let currMin = d.getMinutes();
 
-    if (currHours == inputHours && currMin == inputMin) {
-      audio.play();
-    }
-  }, 1000);
+intervalId=setInterval(() => {
+  let d = new Date();
+  let currHours = d.getHours();
+  let currMin = d.getMinutes();
+
+  if (currHours == inputHours && currMin == inputMin) {
+    audio.play();
+  }
+}, 1000);
 });
+let stop = document.getElementById("stp");
+stop.addEventListener('click', function(){
+    audio.pause();
+  audio.currentTime = 0;
+clearInterval(intervalId);
+})
